117973ce02f43f87301e8a5a06fdab3dc6156feb
bokov <aswer66@mail.ru>
2023-07-23 12:36:22 +0300

    add JSON format

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,16 +1,30 @@
 package server;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class Server {
+
     static ArrayList<User> users = new ArrayList<>();
 
     public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
         // local ip 127.0.0.1 port 9123
         try {
             ServerSocket serverSocket = new ServerSocket(9123);
@@ -25,11 +39,22 @@ public class Server {
                     @Override
                     public void run() {
                         try {
-                            user.getOut().writeUTF("Как тебя зовут?");
-                            String name = user.getIn().readUTF();
-                            user.setName(name);
+                            while (true) {
+                                user.getOut().writeUTF("Для авторизации введите /login \n" +
+                                        "Для регистрации введите /reg \n");
+                                String command = user.getIn().readUTF();
+                                if (command.equals("/reg")) {
+                                    user.reg();
+                                    break;
+                                } else if (command.equals("/login")) {
+                                    if(user.login()) break;
+                                } else {
+                                    user.getOut().writeUTF("Введена не верная команда, повторите ввод");
+                                }
+                            }
                             user.getOut().writeUTF("Добро пожаловать " + user.getName());
                             broadCast(user.getName() + " подключился(лась)");
+                            sendOnlineUsers();
                             while (true) {
                                 String msg = user.getIn().readUTF();
                                 System.out.println(user.getName() + " :  " + msg);
@@ -40,6 +65,7 @@ public class Server {
                             users.remove(user);
                             try {
                                 broadCast("Клиент " + user.getName() + " <-- отключился");
+                                sendOnlineUsers();
                             } catch (IOException ex) {
                                 throw new RuntimeException(ex);
                             }
@@ -58,5 +84,14 @@ public class Server {
         for (User user : users) {
             user.getOut().writeUTF(user.getName() + " -> " + msg);
         }
+    }
+    public static void sendOnlineUsers() throws IOException {
+        JSONArray jsonArray = new JSONArray();
+        for(User user: users){
+            jsonArray.add(user.getName());          // Создаем массив jsonArray с именами пользователей
+        }
+        JSONObject jsonObject = new JSONObject();   // Создаем JSON объект внутри которого лежит массив
+        jsonObject.put("onlineUsers",jsonArray);   // добавляем поле в котором ключ (onlineUsers) / значение = jsonArray
+        broadCast(jsonObject.toJSONString());
     }
 }
--- src/server/User.java
+++ src/server/User.java
@@ -4,6 +4,10 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class User {
     private Socket socket;
@@ -16,9 +20,11 @@ public class User {
         this.out = new DataOutputStream(socket.getOutputStream());
         this.in = new DataInputStream(socket.getInputStream());
     }
+
     public DataOutputStream getOut() {
         return out;
     }
+
     public DataInputStream getIn() {
         return in;
     }
@@ -29,6 +35,57 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public boolean reg() {
+        try {
+            this.getOut().writeUTF(" Введите ваше имя ");
+            String name = this.getIn().readUTF();
+            this.getOut().writeUTF(" Введите login : ");
+            String login = this.getIn().readUTF();
+            this.getOut().writeUTF(" Введите pass : ");
+            String pass = this.getIn().readUTF();
+
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/chat39",
+                    "root",
+                    "");
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(
+                    "INSERT INTO users (name,login,pass) VALUES ('" + name + "','" + login + "','" + pass + "')"
+            );
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return true;
+    }
+    public boolean login(){
+        try {
+        this.getOut().writeUTF(" Введите login : ");
+        String login = this.getIn().readUTF();
+        this.getOut().writeUTF(" Введите pass : ");
+        String pass = this.getIn().readUTF();
+        Connection connection = null;
+
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/chat39",
+                    "root",
+                    "");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(
+                    "SELECT * FROM users WHERE login='" + login + "' AND pass='" + pass + "';");
+            if (resultSet.next()) {
+                String name = resultSet.getString("name");
+                this.setName(name);
+                return true;
+            } else {
+                this.getOut().writeUTF("Неверный логин или пароль");
+                return  false;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
 
